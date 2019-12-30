8ce95f2bbd0a90bf92ba7ea122575ab825c8cbcc
Zach Weber <weberzt@gmail.com>
2019-12-30 23:01:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,7 @@ def roll_call_dwarves(names)
   names.each_with_index {|name, index| puts "#{index+1} #{name}"} 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls# code an argument here
